d3c993b4c1027e9e28a118675405d23ecc6eeb0c
Pedro Dicati <pedro.dicati@estudante.ifms.edu.br>
2022-09-08 10:57:44 -0300

    fix: corrigindo envio de email, agora funcional

--- envia-email.py
+++ envia-email.py
@@ -1,13 +1,37 @@
 import smtplib
 
-from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-msg = EmailMessage()
+email_login = "phgedi48@gmail.com"
+password = 'bfvgemhgmosywrwn'
 
-conection = smtplib.SMTP_SSL('smtp.gmail.com', 456)
-conection.login('phgedi48@gmail.com', "bfvgemhgmosywrwn")
-conection.sendmail(
-    from_addr="",
-    to_addrs="",
-    msg=""
-    )
+to = "pedro.dicati@estudante.ifms.edu.br"
+
+smtp_server = 'smtp.gmail.com'
+smtp_port = 587
+
+message = MIMEMultipart('alternative')
+
+message['Subject'] = "Só um teste"
+message['From'] = email_login
+message['To'] = to
+
+text = "Esse é o texto que vai no email"
+
+message_text = MIMEText(text, 'plain')
+
+message.attach(message_text)
+
+mail = smtplib.SMTP(smtp_server, smtp_port)
+
+print(mail)
+
+mail.ehlo()
+
+mail.starttls()
+
+mail.login(email_login, password)
+
+mail.sendmail(email_login, to, message.as_string())
+mail.quit()
